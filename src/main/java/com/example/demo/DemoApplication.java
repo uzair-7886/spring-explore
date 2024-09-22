f015b7ec54df77db1b74d6040a07257f5e79b69b
Uzair Khan <jerry.khan7886@gmail.com>
2024-09-22 19:27:15 +0500

    init

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		// System.out.println("Hello World");
+		log.info("App Started");
+	}
+}
